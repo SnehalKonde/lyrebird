2af61ecfbf9c98b4183131e1f6c4ff98ab613e21
Snehal Konde <snehal.konde@globant.com>
2017-08-22 15:11:55 +0530

    Initial React Project Structure

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../assets/images/logo.png';
+import './header.css';
+
+class Header extends Component {
+  render() {
+    return (
+        <div className="header">
+          <img src={logo} className="header__logo" alt="logo" />
+        </div>
+    );
+  }
+}
+
+export default Header;
