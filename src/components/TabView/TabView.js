8b20e85ee59dc52b0f0f21c67f2be1c9c5cdd026
Snehal Konde <snehal.konde@globant.com>
2017-08-30 17:12:26 +0530

    Removed tabs from app.js and created seperate file for tabView

---
+++ src/components/TabView/TabView.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Tabs from 'react-tabs-navigation';
+import './tab.css';
+const hashHistory = require('react-router/lib/hashHistory');
+
+class TabView extends Component {
+  constructor(props) {
+      super();
+      this.state = ({
+          selectedTab : props.selectedTab
+      })
+  }
+  setValue(val) {
+      this.setState({
+          selectedTab: val
+      });
+  }
+  onTabChange = (i) => {
+    if(i === 0) {
+      hashHistory.push('/service');
+      this.setValue(0);
+    } else {
+      hashHistory.push('/workflow');
+      this.setValue(1);
+    }
+  }
+  render() {
+    return (
+    <div className="tabView">
+        <Tabs
+            onTabChange = { this.onTabChange }
+            selected = { this.state.selectedTab }
+            tabs={[
+            {
+                children: () => (
+                <div>
+                    {this.props.children}
+                </div>
+                ),
+                displayName: 'SERVICE MOCKING'
+            },
+            {
+                children: () => (
+                <div>
+                    {this.props.children}
+                </div>
+                ),
+                displayName: 'WORKFLOW'
+            }
+            ]}
+            lineStyle ={{ backgroundColor: 'blue', height: 2, display: 'block', transition: 'margin-left 0.25s cubic-bezier(0.15, 0.48, 0.42, 1.13)'}}
+            tabsBarStyle={{backgroundColor: 'rgba(255,255,255,1)', fontSize: 14, height:40, fontFamily:'Calibri, Trebuchet, sans-serif'}}
+            selectedTabStyle ={{':hover': {backgroundColor: 'rgba(255,255,255,1)'}, backgroundColor: 'rgba(255,255,255,1)'}}
+            tabsStyle={{':hover': {backgroundColor: 'rgba(255,255,255,1)'}, color:'#1565C0',paddingTop: 10, ':focus':{boxShadow: 'none'}}}
+        />
+    </div>
+    );
+  }
+}
+
+export default TabView;
--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -1,62 +1,17 @@
 import React, { Component } from 'react';
 import Header from '../../components/HeaderView/header';
-import Tabs from 'react-tabs-navigation';
-import './app.css';
-const hashHistory = require('react-router/lib/hashHistory');
+import TabView from '../../components/TabView/TabView';
 
 class App extends Component {
   constructor(props) {
       super();
-  }
-  onTabChange = (i) => {
-    if(i === 0) {
-      hashHistory.push('/service');
-    } else {
-      hashHistory.push('/workflow');
-    }
+      this.selectedTab = (props.location.pathname === "/workflow") ? 1 : 0
   }
   render() {
-    // Need to revisit logic
-    this.selected = 0;
-    let path = this.props.location.pathname;
-    if(path === '/' || path === '/service') {
-        this.selected = 0;
-    } else if (path === '/workflow') {
-        this.selected = 1;
-    } else {
-        this.selected = 0;
-    }
     return (
         <div>
           <Header />
-          <div className="tabView">
-            <Tabs
-              onTabChange = { this.onTabChange }
-              selected = { this.selected }          
-              tabs={[
-                {
-                  children: () => (
-                    <div>
-                      {this.props.children}
-                    </div>
-                  ),
-                  displayName: 'SERVICE MOCKING'
-                },
-                {
-                  children: () => (
-                    <div>
-                      {this.props.children}
-                    </div>
-                  ),
-                  displayName: 'WORKFLOW'
-                }
-              ]}
-              lineStyle ={{ backgroundColor: 'blue', height: 2, display: 'block', transition: 'margin-left 0.25s cubic-bezier(0.15, 0.48, 0.42, 1.13)'}}
-              tabsBarStyle={{backgroundColor: 'rgba(255,255,255,1)', fontSize: 14, height:40, fontFamily:'Calibri, Trebuchet, sans-serif'}}
-              selectedTabStyle ={{':hover': {backgroundColor: 'rgba(255,255,255,1)'}, backgroundColor: 'rgba(255,255,255,1)'}}
-              tabsStyle={{':hover': {backgroundColor: 'rgba(255,255,255,1)'}, color:'#1565C0',paddingTop: 10, ':focus':{boxShadow: 'none'}}}
-            />
-          </div>
+          <TabView selectedTab={this.selectedTab} children={this.props.children} />
         </div>
     );
   }
